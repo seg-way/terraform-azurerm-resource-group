cfb9e16af8ec9a3aa8d11b2b816c1ee860bed9ea
Ryan Faircloth <ryan@dss-i.com>
2023-01-06 09:22:24 -0500

    fix: outputs and init module

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "azurerm_resource_group" "example" {
+resource "azurerm_resource_group" "instance" {
   name     = var.uniqueName
   location = var.location
 }
--- outputs.tf
+++ outputs.tf
@@ -1,34 +1,6 @@
-output "eks_cluster_name" {
-  value = module.eks.cluster_name
+output "resource_group_name" {
+  value = azurerm_resource_group.instance.name
 }
-output "eks_endpoint" {
-  value = module.eks.cluster_endpoint
+output "resource_group_location" {
+  value = azurerm_resource_group.instance.location
 }
-output "eks_cluster_certificate_authority_data" {
-  value = module.eks.cluster_certificate_authority_data
-}
-output "eks_oidc_provider_arn" {
-  value = module.eks.oidc_provider_arn
-}
-output "eks_cluster_oidc_issuer_url" {
-  value = module.eks.cluster_oidc_issuer_url
-}
-output "karpenter_queue_name" {
-  value = module.karpenter.queue_name
-}
-output "karpenter_instance_profile_name" {
-  value = module.karpenter.instance_profile_name
-}
-
-output "karpenter_irsa_arn" {
-  value = module.karpenter.irsa_arn
-}
-output "cluster_version" {
-  value = module.eks.cluster_version
-}
-# output "eks_karpenter_iam_role_name" {
-#   value = module.eks.eks_managed_node_groups["karpenter"].iam_role_name
-# }
-# output "eks_karpenter_iam_role_arn" {
-#   value = module.eks.eks_managed_node_groups["karpenter"].iam_role_arn
-# }
